215b13b5e3e983d269b25ba628de36e038b23650
xtxn <atchuchev@gmail.com>
2025-05-31 13:08:33 +0300

    SU DOM introduction - exercises

--- SWE-academy/SoftUni-JS-Advanced-May-2025/08-DOM-introduction-Exercise/08. Generate Report/report.js
+++ SWE-academy/SoftUni-JS-Advanced-May-2025/08-DOM-introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,26 @@
 function generateReport() {
-    //TODO
+
+    let result = [];
+
+    const head = document.querySelectorAll('thead tr th');
+    console.log(head);
+
+    const iterate = document.querySelectorAll('tbody tr');
+    let output = document.getElementById('output');
+
+    for (const row of iterate) {
+        output.value = '';
+        let current = {};
+
+        for (let i = 0; i < head.length; i++) {
+            let checkbox = head[i].querySelector('input');
+
+            if (checkbox.checked) {
+                current[head[i].textContent.toLowerCase().trim()] = row.children[i].textContent;
+            }
+        }
+        result.push(current);
+    }
+    output.value = JSON.stringify(result);
+
 }
