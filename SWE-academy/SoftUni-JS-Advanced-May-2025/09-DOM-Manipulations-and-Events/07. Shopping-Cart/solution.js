05156584e60a56fcc66c932c301c69edd48cef00
xtxn <atchuchev@gmail.com>
2025-06-02 16:51:54 +0300

    SU DOM manipulation and events

--- SWE-academy/SoftUni-JS-Advanced-May-2025/09-DOM-Manipulations-and-Events/07. Shopping-Cart/solution.js
+++ SWE-academy/SoftUni-JS-Advanced-May-2025/09-DOM-Manipulations-and-Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,32 @@
 function solve() {
-   //TODO...
+   const eventTarget = document.getElementsByClassName('product');
+   let textArea = document.querySelector('textarea');
+   const checkoutBtn = document.querySelector('.checkout');
+
+   let totalPrice = 0;
+   let finalProducts = [];
+   let allBtn = [...document.getElementsByTagName('button')];
+
+   for (const product of eventTarget) {
+      product.addEventListener('click', (e) => {
+         if (e.target.classList.contains('add-product')) {
+            let productName = product.querySelector('.product-title');
+            let productPrice = product.querySelector('.product-line-price');
+
+            totalPrice += Number(productPrice.textContent);
+            if (!finalProducts.includes(productName.textContent)) {
+               finalProducts.push(productName.textContent);
+            }
+            textArea.textContent += `Added ${productName.textContent} for ${productPrice.textContent} to the cart.\n`;
+         }
+      });
+   }
+
+   checkoutBtn.addEventListener('click', () => {
+      textArea.textContent += `You bought ${finalProducts.join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+      allBtn.forEach((button, i) => {
+         button.disabled = true;
+      });
+   });
 }
