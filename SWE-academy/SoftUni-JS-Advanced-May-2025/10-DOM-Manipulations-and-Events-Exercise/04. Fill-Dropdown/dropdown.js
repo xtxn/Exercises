13cb1a3ea4b0b601af230b02bbba99989ed2e5a2
xtxn <atchuchev@gmail.com>
2025-06-04 17:55:53 +0300

    SU DOM manipulation and events - exercises

--- SWE-academy/SoftUni-JS-Advanced-May-2025/10-DOM-Manipulations-and-Events-Exercise/04. Fill-Dropdown/dropdown.js
+++ SWE-academy/SoftUni-JS-Advanced-May-2025/10-DOM-Manipulations-and-Events-Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInput = document.getElementById('newItemText');
+    const valueInput = document.getElementById('newItemValue');
+    let option = document.createElement('option');
+
+    // let text = textInput.value;
+    // let value = valueInput.value;
+    if (textInput.value == '' || valueInput.value == '') {
+        return;
+    }
+    option.value = valueInput.value;
+    option.textContent = textInput.value;
+
+    const dropdownInput = document.getElementById('menu');
+    dropdownInput.appendChild(option);
+    textInput.value = '';
+    valueInput.value = '';
 }
