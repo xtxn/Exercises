46fb713bfc533590d4359663ebbc4ae607984c20
xtxn <atchuchev@gmail.com>
2025-06-06 15:52:34 +0300

    SU DOM manipulation and events - exercises

--- SWE-academy/SoftUni-JS-Advanced-May-2025/10-DOM-Manipulations-and-Events-Exercise/05. Encode-and-Decode-Messages/app.js
+++ SWE-academy/SoftUni-JS-Advanced-May-2025/10-DOM-Manipulations-and-Events-Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const inputs = document.querySelectorAll('textarea');
+    const encodeInput = inputs[0];
+    const decodeInput = inputs[1];
+
+    const btns = document.querySelectorAll('button')
+    const encodeBtn = btns[0];
+    const decodeBtn = btns[1];
+
+    encodeBtn.addEventListener('click', () => {
+        let result = '';
+        const textToEncode = encodeInput.value;
+        for (const symbol of textToEncode) {
+            let symboAsci = symbol.charCodeAt();
+            result += String.fromCharCode(symboAsci + 1);
+        }
+        decodeInput.value = result;
+        encodeInput.value = '';
+    });
+
+    decodeBtn.addEventListener('click', () => {
+        let result = '';
+        const textToDecode = decodeInput.value;
+        for (const symbol of textToDecode) {
+            let symbolAsci = symbol.charCodeAt();
+            result += String.fromCharCode(symbolAsci - 1);
+        }
+        decodeInput.value = result;
+    });
 }
