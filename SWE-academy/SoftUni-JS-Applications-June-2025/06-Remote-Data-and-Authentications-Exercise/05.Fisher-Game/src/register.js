cc6ae71f6e3fcd0b90091a32ddc9323a8cc5d339
xtxn <atchuchev@gmail.com>
2025-07-16 00:17:17 +0300

    Fifth task - register logic

--- SWE-academy/SoftUni-JS-Applications-June-2025/06-Remote-Data-and-Authentications-Exercise/05.Fisher-Game/src/register.js
+++ SWE-academy/SoftUni-JS-Applications-June-2025/06-Remote-Data-and-Authentications-Exercise/05.Fisher-Game/src/register.js
@@ -1,6 +1,49 @@
 function register(params) {
-    console.log('register');
+    const form = document.querySelector('form#register');
+    const url = 'http://localhost:3030/users/register';
 
-};
+    form.addEventListener('submit', onRegister);
 
-register();
+    async function onRegister(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+
+        const email = data.email;
+        const password = data.password;
+        const rePass = data.rePass;
+
+        if (password != rePass) {
+            alert(`Please enter matching password and repeat`);
+            return;
+        };
+        if (!email || !password || !rePass) {
+            alert('Please fill all input fields');
+            return;
+        };
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/JSON'
+            },
+            body: JSON.stringify({ email, password })
+        };
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error('Cannot connect to server');
+            };
+            const userData = await response.json();
+
+            sessionStorage.setItem('userData', JSON.stringify(userData));
+            window.location = 'index.html'
+        } catch (error) {
+            alert('Cannot connect to server')
+        };
+    };
+
+}
+
+register(); 
