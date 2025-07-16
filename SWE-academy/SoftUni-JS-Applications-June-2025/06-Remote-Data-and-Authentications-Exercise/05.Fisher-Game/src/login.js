dab6adea1ba05ffc7a6d4c20b9a4168dfdc46c48
xtxn <atchuchev@gmail.com>
2025-07-16 13:19:58 +0300

    Fifth task - login logic

--- SWE-academy/SoftUni-JS-Applications-June-2025/06-Remote-Data-and-Authentications-Exercise/05.Fisher-Game/src/login.js
+++ SWE-academy/SoftUni-JS-Applications-June-2025/06-Remote-Data-and-Authentications-Exercise/05.Fisher-Game/src/login.js
@@ -1,6 +1,37 @@
 function login(params) {
-    console.log('login');
+    const btnNavLogin = document.getElementById('login');
+    const form = document.querySelector('form#login');
+    const inputEmail = document.querySelector('input[name="email"]');
+    const inputPass = document.querySelector('input[name="password"]');
+    const url = 'http://localhost:3030/users/login';
 
+    form.addEventListener('submit', onLogin);
+
+    async function onLogin(event) {
+        event.preventDefault();
+
+        if (!inputEmail.value || !inputPass.value) {
+            return;
+        };
+        const formData = new FormData(event.target)
+        const data = Object.fromEntries(formData.entries());
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        }
+
+        let response = await fetch(url, options);
+        const userData = await response.json();
+
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+        window.location = 'index.html';
+
+
+    };
 };
 
 login();
