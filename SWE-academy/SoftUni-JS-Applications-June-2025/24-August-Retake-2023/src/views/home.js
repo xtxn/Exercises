600595d9fed93802345ca2e63e6a1592ca191938
xtxn <atchuchev@gmail.com>
2025-08-07 17:19:09 +0300

    home view

---
+++ SWE-academy/SoftUni-JS-Applications-June-2025/24-August-Retake-2023/src/views/home.js
@@ -0,0 +1,14 @@
+import { html } from "../lib.js";
+
+const homeTemplate = () => html`
+<section id="home">
+    <h1>
+        Welcome to <span>Samurider</span> moto market, your premier destination for Japanese motorcycles.
+    </h1>
+    <img src="./images/motorcycle.png" alt="home" />
+</section>
+`;
+
+export function homeView(ctx) {
+    ctx.render(homeTemplate());
+}
