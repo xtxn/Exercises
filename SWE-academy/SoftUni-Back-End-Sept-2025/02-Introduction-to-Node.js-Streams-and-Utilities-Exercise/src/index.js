a4b9ac589a914f0e77038c9f31aaff2173faaa3e
xtxn <atchuchev@gmail.com>
2025-09-13 23:43:03 +0300

    Handle addBreed

--- SWE-academy/SoftUni-Back-End-Sept-2025/02-Introduction-to-Node.js-Streams-and-Utilities-Exercise/src/index.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/02-Introduction-to-Node.js-Streams-and-Utilities-Exercise/src/index.js
@@ -2,13 +2,20 @@ import { v4 as uuidv4 } from 'uuid';
 
 import http from 'http';
 import fs from 'fs/promises'
+import { log } from 'console';
 
 const server = http.createServer(async (req, res) => {
 
     const location = req.url;
 
-    // Home Page
-    if (location === '/') {
+    // Load CSS
+    if (location === '/styles/site.css') {
+        res.writeHead(200, { 'content-type': 'text/css' })
+        const html = await fs.readFile(`./src/content/styles/site.css`);
+        res.write(html.toString());
+        res.end();
+
+    } else if (location === '/') {
         const templateHtml = await fs.readFile('./src/views/index.html', 'utf8');
         const catsData = await fs.readFile('./src/dbCats.json', 'utf8');
         const cats = JSON.parse(catsData)
@@ -30,13 +37,11 @@ const server = http.createServer(async (req, res) => {
 
         res.writeHead(200, { 'content-type': 'text/html' });
         res.write(finalHtml);
-    }
+        res.end();
 
-    // Add Cat
-    if (location === '/cats/add-cat') {
+    } else if (location === '/cats/add-cat') {
         const templateHtml = await fs.readFile('./src/views/addCat.html', 'utf8');
-        const breedData = await fs.readFile('./src/dbBreeds.json');
-        const breeds = JSON.parse(breedData);
+        const breeds = await getAllBreeds();;
 
         const breedsTemplate = breeds.map(breed =>
             `<option value="${breed}">${breed}</option>`
@@ -45,21 +50,54 @@ const server = http.createServer(async (req, res) => {
         const finalHtml = templateHtml.replace('{{breeds}}', breedsTemplate);
 
         res.writeHead(200, { 'content-type': 'text/html' });
-        res.write(finalHtml)
-    }
+        res.write(finalHtml);
+        res.end();
 
-    if (location === '/styles/site.css') {
-        res.writeHead(200, { 'content-type': 'text/css' })
-        const html = await fs.readFile(`./src/content/styles/site.css`);
-        res.write(html.toString())
+    } else if (location === '/cats/add-breed') {
+
+        if (req.method === 'GET') {
+            const templateHtml = await fs.readFile('./src/views/addBreed.html', 'utf8');
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.write(templateHtml)
+            res.end();
+
+        } else if (req.method === 'POST') {
+
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+
+            req.on('end', (async () => {
+                const formData = new URLSearchParams(body);
+                const breedName = formData.get('breed');
+
+                if (breedName) {
+                    await addBreed(breedName);
+                }
+
+                res.writeHead(302, { 'Location': '/' })
+                res.end();
+            }))
+        }
     }
-    res.end();
 });
 
 server.listen('3000', () => {
     console.log('Server started at http://localhost:3000');
 });
 
-async function addBreed() {
-
+async function getAllBreeds() {
+    const breedData = await fs.readFile('./src/dbBreeds.json');
+    const breedArr = JSON.parse(breedData);
+    return breedArr;
 }
+
+async function addBreed(breed) {
+    const breeds = await getAllBreeds();
+    breeds.push(breed);
+    await fs.writeFile('./src/dbBreeds.json', JSON.stringify(breeds), 'utf8');
+}
+
+
+
