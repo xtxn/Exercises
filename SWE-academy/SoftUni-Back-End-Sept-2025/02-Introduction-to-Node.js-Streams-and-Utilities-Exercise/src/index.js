119fc20cdd0457bcb6f5df5e9c365e875e0718b1
xtxn <atchuchev@gmail.com>
2025-09-14 23:51:34 +0300

    add cat functionality and helper cat functions

--- SWE-academy/SoftUni-Back-End-Sept-2025/02-Introduction-to-Node.js-Streams-and-Utilities-Exercise/src/index.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/02-Introduction-to-Node.js-Streams-and-Utilities-Exercise/src/index.js
@@ -15,44 +15,49 @@ const server = http.createServer(async (req, res) => {
         res.write(html.toString());
         res.end();
 
+        // Load Home Page
     } else if (location === '/') {
-        const templateHtml = await fs.readFile('./src/views/index.html', 'utf8');
-        const catsData = await fs.readFile('./src/dbCats.json', 'utf8');
-        const cats = JSON.parse(catsData)
-
-        const catCards = cats.map(cat => `
-             <li>
-                    <img src="${cat.imageUrl}" alt="Black Cat">
-                    <h3>${cat.name}</h3>
-                    <p><span>Breed: </span>${cat.breed}</p>
-                    <p><span>Description: </span>${cat.description}</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-            `).join('\n');
-
-        const finalHtml = templateHtml.replace('{{cats}}', catCards);
-
+        const cats = await getAllCats();
         res.writeHead(200, { 'content-type': 'text/html' });
-        res.write(finalHtml);
+        res.write(await displayAllCats(cats));
         res.end();
 
+        // Add Cat
     } else if (location === '/cats/add-cat') {
-        const templateHtml = await fs.readFile('./src/views/addCat.html', 'utf8');
-        const breeds = await getAllBreeds();;
+        if (req.method === "GET") {
+            const templateHtml = await fs.readFile('./src/views/addCat.html', 'utf8');
+            const breeds = await getAllBreeds();;
 
-        const breedsTemplate = breeds.map(breed =>
-            `<option value="${breed}">${breed}</option>`
-        ).join('\n');
+            const breedsTemplate = breeds.map(breed =>
+                `<option value="${breed}">${breed}</option>`
+            ).join('\n');
 
-        const finalHtml = templateHtml.replace('{{breeds}}', breedsTemplate);
+            const finalHtml = templateHtml.replace('{{breeds}}', breedsTemplate);
 
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.write(finalHtml);
-        res.end();
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.write(finalHtml);
+            res.end();
 
+        } else if (req.method === 'POST') {
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+
+            req.on('end', () => {
+                const formData = new URLSearchParams(body);
+                const currentCat = Object.fromEntries(formData);
+                const newCat = {
+                    id: uuidv4(),
+                    ...currentCat
+                }
+
+                addCat(newCat);
+                res.writeHead(302, { 'Location': '/' });
+                res.end();
+            })
+        }
+        // Add breed
     } else if (location === '/cats/add-breed') {
 
         if (req.method === 'GET') {
@@ -99,5 +104,36 @@ async function addBreed(breed) {
     await fs.writeFile('./src/dbBreeds.json', JSON.stringify(breeds), 'utf8');
 }
 
+async function getAllCats() {
+    const catsData = await fs.readFile('./src/dbCats.json', 'utf8');
+    return JSON.parse(catsData);
+}
+
+async function displayAllCats(cats) {
+    const templateHtml = await fs.readFile('./src/views/index.html', 'utf8');
+    // const cats = await getAllCats();
+
+    const catCards = cats.map(cat => `
+             <li>
+                    <img src="${cat.imageUrl}" alt="Black Cat">
+                    <h3>${cat.name}</h3>
+                    <p><span>Breed: </span>${cat.breed}</p>
+                    <p><span>Description: </span>${cat.description}</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+            `).join('\n');
+
+    return templateHtml.replace('{{cats}}', catCards);
+}
+
+async function addCat(newCat) {
+    const allCats = await getAllCats();
+    allCats.push(newCat);
+
+    await fs.writeFile('./src/dbCats.json', JSON.stringify(allCats, null, 2), 'utf8')
+};
 
 
