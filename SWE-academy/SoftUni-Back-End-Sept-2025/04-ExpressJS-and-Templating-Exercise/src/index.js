4ffa14281ea895b81459d495b48789088770fffc
xtxn <atchuchev@gmail.com>
2025-09-20 23:49:53 +0300

    Add static files

--- SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/index.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/index.js
@@ -12,6 +12,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
+// Setup middlewares
+app.use(express.static('src/public'));
+
 // Routes
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
