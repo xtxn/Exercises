eff782d330db38bc54a1369b10d7fc8aab87aa82
xtxn <atchuchev@gmail.com>
2025-09-20 23:40:36 +0300

    Add express-handlebars

--- SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/index.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
+const port = 3000;
 
+// Setup Handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('It Works!');
+    res.render('home', { layout: false });
 });
 
-app.listen(3000, () => console.log('Server is listening on http://localhost:3000'));
+// Start server
+app.listen(port, () => console.log('Server is listening on http://localhost:3000'));
