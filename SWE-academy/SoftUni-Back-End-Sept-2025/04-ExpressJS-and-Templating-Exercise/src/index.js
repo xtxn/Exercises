f570f80a549d0404b06e835b82d8e4c98ea52921
xtxn <atchuchev@gmail.com>
2025-09-23 12:37:35 +0300

    Add views layout and about page

--- SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/index.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/index.js
@@ -8,8 +8,8 @@ const port = 3000;
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
-
 app.set('view engine', 'hbs');
+
 app.set('views', 'src/views');
 
 // Setup middlewares
@@ -17,8 +17,12 @@ app.use(express.static('src/public'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 // Start server
 app.listen(port, () => console.log('Server is listening on http://localhost:3000'));
