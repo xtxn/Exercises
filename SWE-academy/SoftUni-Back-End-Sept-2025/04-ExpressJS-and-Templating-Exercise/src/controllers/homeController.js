6857b45c212ebfdd3f90102bb25c870e53177b2c
xtxn <atchuchev@gmail.com>
2025-09-24 17:47:31 +0300

    Render movies on home page

--- SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/controllers/homeController.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/controllers/homeController.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import movieService from "../services/movieService.js";
 
 export const homeController = Router();
 
-homeController.get('/', (req, res) => {
-    res.render('home');
+homeController.get('/', async (req, res) => {
+    const movies = await movieService.getAllMovies();
+    console.log(movies);
+
+    res.render('home', { movies });
 });
 
 homeController.get('/about', (req, res) => {
