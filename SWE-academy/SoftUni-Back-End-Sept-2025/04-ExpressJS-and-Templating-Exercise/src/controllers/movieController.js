ef6b697d1b2e86a01bf4b25f6b13f771c7c80c7a
xtxn <atchuchev@gmail.com>
2025-09-24 22:51:10 +0300

    Add body parser

--- SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/controllers/movieController.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/04-ExpressJS-and-Templating-Exercise/src/controllers/movieController.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
+import { v4 as uuidv4 } from 'uuid';
+import Movie from "../models/Movie.js";
 
 export const movieController = Router();
 
 movieController.get('/create', (req, res) => {
     res.render('create');
 });
+
+movieController.post('/create', (req, res) => {
+    const addedMovie = req.body;
+    addedMovie._id = uuidv4();
+    console.log(addedMovie);
+
+    res.redirect('/')
+})
