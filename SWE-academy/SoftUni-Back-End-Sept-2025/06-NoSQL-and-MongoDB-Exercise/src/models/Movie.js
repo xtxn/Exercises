09680067d202c7953f9b0931b5e6806badc71a96
xtxn <atchuchev@gmail.com>
2025-09-28 18:10:06 +0300

    Add mongoose movie model

--- SWE-academy/SoftUni-Back-End-Sept-2025/06-NoSQL-and-MongoDB-Exercise/src/models/Movie.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/06-NoSQL-and-MongoDB-Exercise/src/models/Movie.js
@@ -1,42 +1,16 @@
-import fs from 'fs/promises';
-import { v4 as uuidv4 } from 'uuid';
+import { Schema, model } from "mongoose";
 
-let dbSerialized = await fs.readFile('./src/db.json', { encoding: 'utf-8' });
-let db = JSON.parse(dbSerialized);
+const movieSchema = new Schema({
+    title: String,
+    category: String,
+    genre: String,
+    director: String,
+    year: Number,
+    imageUrl: String,
+    rating: Number,
+    description: String,
+});
 
-export default class Movie {
-    constructor(data) {
-        Object.assign(this, data)
-        this._id = uuidv4();
-    }
+const Movie = model('Movie', movieSchema);
 
-    static find(filter = {}) {
-        let result = db.movies.slice();
-        if (filter.title) {
-            result = result.filter(movie => movie.title.toLowerCase().includes(filter.title.toLowerCase()));
-        }
-        if (filter.genre) {
-            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase())
-        }
-        if (filter.year) {
-            result = result.filter(movie => movie.year === filter.year)
-        }
-
-        return result;
-    }
-
-    static findOne(movieId) {
-        let result;
-        if (movieId) {
-            result = db.movies.find(movie => movie._id === movieId);
-        }
-        return result;
-    }
-
-    async save() {
-        db.movies.push(this);
-        const dbWrite = JSON.stringify(db, null, 2);
-        await fs.writeFile('./src/db.json', dbWrite);
-        return this;
-    }
-}
+export default Movie;
