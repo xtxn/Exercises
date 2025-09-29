2523c54bfced1b816509babd5fd22b4450bb2fa2
xtxn <atchuchev@gmail.com>
2025-09-29 18:39:21 +0300

    use lean()

--- SWE-academy/SoftUni-Back-End-Sept-2025/06-NoSQL-and-MongoDB-Exercise/src/services/movieService.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/06-NoSQL-and-MongoDB-Exercise/src/services/movieService.js
@@ -1,18 +1,20 @@
 import Movie from "../models/Movie.js";
 
 async function getAllMovies(filter) {
-    // const result = await Movie.find(filter).lean();
-    const result = await Movie.find(filter)
+    const result = await Movie.find(filter).lean();
+    // const result = await Movie.find(filter)
     return result;
 };
 
 async function createMovie(movieData) {
-    const movie = new Movie(movieData);
-    await movie.save();
+    // const movie = new Movie(movieData);
+    // await movie.save();
+
+    return await Movie.create(movieData)
 }
 
 async function getOneMovie(movieId) {
-    return await Movie.findOne(movieId);
+    return await Movie.findOne({ _id: movieId }).lean();
 }
 
 export default {
