4037bdf89b4104ebfe5bad4037b51067da55a959
xtxn <atchuchev@gmail.com>
2025-10-07 23:39:47 +0300

    Hash password before save

--- SWE-academy/SoftUni-Back-End-Sept-2025/08-Session-and-Authentication-Exercise/src/models/User.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/08-Session-and-Authentication-Exercise/src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     email: {
@@ -10,6 +11,11 @@ const userSchema = new Schema({
         required: true,
     }
 })
+
+userSchema.pre('save', async function () {
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
 const User = model('User', userSchema);
 
