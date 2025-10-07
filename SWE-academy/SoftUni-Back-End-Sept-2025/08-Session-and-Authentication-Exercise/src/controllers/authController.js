0b733f84a8b1c72955a6a6c76e98ca25d4aeca00
xtxn <atchuchev@gmail.com>
2025-10-08 00:58:30 +0300

    Add login page

--- SWE-academy/SoftUni-Back-End-Sept-2025/08-Session-and-Authentication-Exercise/src/controllers/authController.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/08-Session-and-Authentication-Exercise/src/controllers/authController.js
@@ -15,4 +15,8 @@ authController.post('/register', async (req, res) => {
     res.redirect('/');
 });
 
+authController.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
 export default authController;
