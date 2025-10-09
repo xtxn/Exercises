43ce7dd67ca6a24263b95c13457cc72c4f7a6428
xtxn <atchuchev@gmail.com>
2025-10-09 17:10:39 +0300

    Clear cookies

--- SWE-academy/SoftUni-Back-End-Sept-2025/08-Session-and-Authentication-Exercise/src/controllers/authController.js
+++ SWE-academy/SoftUni-Back-End-Sept-2025/08-Session-and-Authentication-Exercise/src/controllers/authController.js
@@ -28,4 +28,9 @@ authController.post('/login', async (req, res) => {
     res.redirect('/');
 });
 
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+})
+
 export default authController;
